405d8804bf5ce4899c2e6ebe007e6e5083d8332b
Justin Yoo <justin.yoojh@gmail.com>
2019-01-31 00:06:41 +1100

    Add tests

---
+++ test/Aliencube.AzureFunctions.Extensions.OpenApi.Tests/Attributes/OpenApiOperationAttributeTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+using Aliencube.AzureFunctions.Extensions.OpenApi.Attributes;
+
+using FluentAssertions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aliencube.AzureFunctions.Extensions.OpenApi.Tests.Attributes
+{
+    [TestClass]
+    public class OpenApiOperationAttributeTests
+    {
+        [TestMethod]
+        public void Given_Value_Properties_Should_Return_Value()
+        {
+            var opId = "Lorem Ipsum";
+            var tag1 = "hello";
+            var tag2 = "world";
+            var attribute = new OpenApiOperationAttribute(opId, tag1, tag2);
+
+            attribute.OperationId.Should().BeEquivalentTo(opId);
+            attribute.Tags.First().Should().BeEquivalentTo(tag1);
+            attribute.Tags.Last().Should().BeEquivalentTo(tag2);
+        }
+    }
+}
