1abca89b1e28bfd1ec856aae2159920bfc4a3956
Kelvin Brokstek-Van Manen <kelvin@kelvinvm.com>
2019-06-25 12:40:50 -0500

    Add missing test files

---
+++ test/Aliencube.AzureFunctions.Tests.Fakes/IFakeInheritedInterface.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Aliencube.AzureFunctions.Tests.Fakes
+{
+    public interface IFakeInheritedInterface : IFakeBaseInterface
+    {
+        DateTime Birthdate { get; set; }
+        double Age { get; set; }
+    }
+}
