bae2984af63462f09bd7d077932f04b6953841c1
Justin Yoo <justin.yoojh@gmail.com>
2020-05-12 19:15:42 +0900

    Update SampleHttpTrigger

--- src/Aliencube.AzureFunctions.FunctionAppV3/SampleHttpTrigger.cs
+++ src/Aliencube.AzureFunctions.FunctionAppV3/SampleHttpTrigger.cs
@@ -46,7 +46,7 @@ namespace Aliencube.AzureFunctions.FunctionAppV3
         [OpenApiParameter(name: "category", In = ParameterLocation.Path, Required = true, Type = typeof(string), Summary = "The category parameter", Visibility = OpenApiVisibilityType.Advanced)]
         [OpenApiResponseBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(SampleResponseModel), Summary = "Sample response")]
         public async Task<IActionResult> GetPath(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "samples/{id:int}/categories/{category:regex(^[a-z]{{3,}}$)}")] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "paths/{id:int}/categories/{category:regex(^[a-z]{{3,}}$)}")] HttpRequest req,
             int id,
             string category,
             ILogger log)
@@ -70,9 +70,7 @@ namespace Aliencube.AzureFunctions.FunctionAppV3
         [OpenApiParameter(name: "limit", In = ParameterLocation.Query, Required = false, Type = typeof(int), Description = "The number of samples to return")]
         [OpenApiResponseBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(SampleResponseModel), Summary = "Sample response")]
         public async Task<IActionResult> GetQuery(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "samples")] HttpRequest req,
-            int id,
-            string category,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "queries")] HttpRequest req,
             ILogger log)
         {
             var result = await this._function.AddLogger(log)
@@ -90,11 +88,10 @@ namespace Aliencube.AzureFunctions.FunctionAppV3
         /// <returns><see cref="SampleResponseModel"/> instance.</returns>
         [FunctionName(nameof(SampleHttpTrigger.GetListResponse))]
         [OpenApiOperation(operationId: "list", tags: new[] { "sample" }, Summary = "Gets the list of samples", Description = "This gets the list of samples.", Visibility = OpenApiVisibilityType.Important)]
+        [OpenApiResponseBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(SampleResponseModel), Summary = "Sample response")]
         [OpenApiResponseBody(statusCode: HttpStatusCode.Created, contentType: "application/json", bodyType: typeof(List<SampleItemModel>), Summary = "Sample response of a List")]
         public async Task<IActionResult> GetListResponse(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "lists")] HttpRequest req,
-            int id,
-            string category,
             ILogger log)
         {
             var result = await this._function.AddLogger(log)
@@ -112,11 +109,10 @@ namespace Aliencube.AzureFunctions.FunctionAppV3
         /// <returns><see cref="SampleResponseModel"/> instance.</returns>
         [FunctionName(nameof(SampleHttpTrigger.GetIListResponse))]
         [OpenApiOperation(operationId: "ilist", tags: new[] { "sample" }, Summary = "Gets the list of samples", Description = "This gets the list of samples.", Visibility = OpenApiVisibilityType.Important)]
+        [OpenApiResponseBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(SampleResponseModel), Summary = "Sample response")]
         [OpenApiResponseBody(statusCode: HttpStatusCode.Accepted, contentType: "application/json", bodyType: typeof(IList<SampleResponseModel>), Summary = "Sample response of a IList")]
         public async Task<IActionResult> GetIListResponse(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "lists")] HttpRequest req,
-            int id,
-            string category,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "ilists")] HttpRequest req,
             ILogger log)
         {
             var result = await this._function.AddLogger(log)
@@ -147,8 +143,6 @@ namespace Aliencube.AzureFunctions.FunctionAppV3
         //[OpenApiResponseBody(statusCode: HttpStatusCode.BadGateway, contentType: "application/json", bodyType: typeof(SampleGenericResponseModel<SampleResponseModel>), Summary = "Sample response of a Generic")]
         //public async Task<IActionResult> GetListResponse(
         //    [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "lists")] HttpRequest req,
-        //    int id,
-        //    string category,
         //    ILogger log)
         //{
         //    var result = await this._function.AddLogger(log)
