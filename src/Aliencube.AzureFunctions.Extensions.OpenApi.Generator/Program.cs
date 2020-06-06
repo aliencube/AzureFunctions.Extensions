ed3f27d4eed09e4fd1c3988ed26f2bc76177bb9b
Justin Yoo <justin.yoojh@gmail.com>
2020-06-07 08:45:15 +0900

    Add console app

---
+++ src/Aliencube.AzureFunctions.Extensions.OpenApi.Generator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+using Microsoft.Build.Construction;
+using Microsoft.Build.Evaluation;
+
+namespace Aliencube.AzureFunctions.Extensions.OpenApi.Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var project = new Project(@"Y:\Documents\devs\ac\AzureFunctions.Extensions\src\Aliencube.AzureFunctions.FunctionAppV3\Aliencube.AzureFunctions.FunctionAppV3.csproj");
+            var pg = project.Properties.OfType<ProjectPropertyGroupElement>().First();
+            var pgps = pg.Properties;
+        }
+    }
+}
