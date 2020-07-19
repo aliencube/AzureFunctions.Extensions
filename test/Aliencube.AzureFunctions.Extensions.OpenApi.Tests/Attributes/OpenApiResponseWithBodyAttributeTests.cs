d95cb58477efbd32876045987dc5a3acf5e1a39e
Justin Yoo <justin.yoojh@gmail.com>
2020-07-19 10:12:50 +0900

    Update tests

--- test/Aliencube.AzureFunctions.Extensions.OpenApi.Tests/Attributes/OpenApiResponseWithBodyAttributeTests.cs
+++ test/Aliencube.AzureFunctions.Extensions.OpenApi.Tests/Attributes/OpenApiResponseWithBodyAttributeTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Net;
 
 using Aliencube.AzureFunctions.Extensions.OpenApi.Attributes;
@@ -10,17 +10,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Aliencube.AzureFunctions.Extensions.OpenApi.Tests.Attributes
 {
     [TestClass]
-    public class OpenApiResponseBodyAttributeTests
+    public class OpenApiResponseWithBodyAttributeTests
     {
         [TestMethod]
         public void Given_Null_Constructor_Should_Throw_Exception()
         {
             var statusCode = HttpStatusCode.OK;
 
-            Action action = () => new OpenApiResponseBodyAttribute(statusCode, null, null);
+            Action action = () => new OpenApiResponseWithBodyAttribute(statusCode, null, null);
             action.Should().Throw<ArgumentNullException>();
 
-            action = () => new OpenApiResponseBodyAttribute(statusCode, "hello world", null);
+            action = () => new OpenApiResponseWithBodyAttribute(statusCode, "hello world", null);
             action.Should().Throw<ArgumentNullException>();
         }
 
@@ -30,11 +30,12 @@ namespace Aliencube.AzureFunctions.Extensions.OpenApi.Tests.Attributes
             var statusCode = HttpStatusCode.OK;
             var contentType = "Hello World";
             var bodyType = typeof(object);
-            var attribute = new OpenApiResponseBodyAttribute(statusCode, contentType, bodyType);
+            var attribute = new OpenApiResponseWithBodyAttribute(statusCode, contentType, bodyType);
 
             attribute.StatusCode.Should().Be(statusCode);
             attribute.ContentType.Should().BeEquivalentTo(contentType);
             attribute.BodyType.Should().Be(bodyType);
+            attribute.Summary.Should().BeNullOrWhiteSpace();
             attribute.Description.Should().BeNullOrWhiteSpace();
         }
     }
