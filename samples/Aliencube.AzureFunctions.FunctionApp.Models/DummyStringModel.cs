cd09b9dc76a9cb0e425f700763c4c572eb435310
Justin Yoo <justin.yoojh@gmail.com>
2020-08-25 11:23:52 +0900

    Update dummy model to verify the URI object

--- samples/Aliencube.AzureFunctions.FunctionApp.Models/DummyStringModel.cs
+++ samples/Aliencube.AzureFunctions.FunctionApp.Models/DummyStringModel.cs
@@ -1,7 +1,11 @@
+using System;
+
 namespace Aliencube.AzureFunctions.FunctionApp.Models
 {
     public class DummyStringModel
     {
         public string StringValue { get; set; }
+
+        public Uri UriValue { get; set; }
     }
 }
